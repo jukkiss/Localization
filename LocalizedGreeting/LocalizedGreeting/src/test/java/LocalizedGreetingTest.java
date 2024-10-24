db6c64af705223b31c49939594f015a6749d8289
Jukka <savela.j@gmail.com>
2024-10-24 10:40:39 +0300

    tests work

--- LocalizedGreeting/LocalizedGreeting/src/test/java/LocalizedGreetingTest.java
+++ LocalizedGreeting/LocalizedGreeting/src/test/java/LocalizedGreetingTest.java
@@ -1,9 +1,18 @@
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
 import java.util.Locale;
 import java.util.ResourceBundle;
 import static org.junit.Assert.assertEquals;
 
+
 public class LocalizedGreetingTest {
+    private ResourceBundle messages;
+
+    @BeforeEach
+    public void setUp() {
+        //No specific setup requires
+    }
 
     @Test
     public void testEnglishGreeting() {
@@ -27,6 +36,22 @@ public class LocalizedGreetingTest {
         ResourceBundle messages = ResourceBundle.getBundle("Message", locale);
         String greeting = messages.getString("greeting");
         assertEquals("Bonjour", greeting);
+    }
+
+    @Test
+    public void testFarsiGreeting() {
+        Locale locale = new Locale("fa", "IR");
+        ResourceBundle messages = ResourceBundle.getBundle("Message", locale);
+        String greeting = messages.getString("greeting");
+        assertEquals("سلام", greeting);
+    }
+
+    @Test
+    public void testJapaneseGreeting() {
+        Locale locale = new Locale("ja", "JP");
+        ResourceBundle messages = ResourceBundle.getBundle("Message", locale);
+        String greeting = messages.getString("greeting");
+        assertEquals("こんにちは", greeting);
     }
 
     @Test
